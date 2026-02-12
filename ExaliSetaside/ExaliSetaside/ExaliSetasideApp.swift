ac1a480cae7337844dc5838997196d5023993d01
Anton Mishkevich <misant1995@gmail.com>
2026-02-12 18:52:58 +0300

    init

---
+++ ExaliSetaside/ExaliSetaside/ExaliSetasideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExaliSetasideApp.swift
+//  ExaliSetaside
+//
+//  Created by mac on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExaliSetasideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
